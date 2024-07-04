880f3ea2fdb2aa6761dc1a6aa25e3e2898ea6e30
mumu <mumushy850421@gmail.com>
2024-07-05 02:51:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,29 +5,8 @@ import { Database } from 'firebase-firestore-lite';
 // 加載 .env 文件中的環境變量
 dotenv.config();
 
-// Firebase 配置
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
-};
-
-const fireWorkersConfig = {
-  uid: process.env.UID,
-  project_id: process.env.PROJECT_ID,
-  client_email: process.env.CLIENT_EMAIL,
-  private_key: process.env.PRIVATE_KEY,
-  private_key_id: process.env.PRIVATE_KEY_ID
-};
-
 // 初始化 Firebase
 const projectId = 'mntlhunter-e8455';
-if (!projectId) {
-	throw new Error('Invalid projectId. Please check your .env file.');
-  }
 const db = new Database({ projectId: projectId });
 
 addEventListener('fetch', event => {
@@ -44,10 +23,7 @@ async function handleRequest(request) {
   // 驗證簽名
   if (verifySignature(signature, message, address)) {
     try {
-		console.log("000")
       const userDocRef = db.ref('users/' + address);
-	  console.log("11111")
-	  console.log(userDocRef)
 	  try{
 		const userDoc = await userDocRef.get();
 		if (userDoc) {
